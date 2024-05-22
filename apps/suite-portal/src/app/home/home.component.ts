4d3bdbfea88fe85d456cf806f4b6fdf1ea2e70ef
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-05-21 21:12:23 -0400

    Build : create form group with validations

--- apps/suite-portal/src/app/home/home.component.ts
+++ apps/suite-portal/src/app/home/home.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ALL_SERVICE_TYPES } from '@suiteportal/api-interfaces';
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'pm-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   serviceTypes = ALL_SERVICE_TYPES;
-
-  constructor() {
-    //
+  form: FormGroup;
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.createForm();
   }
 
   ngOnInit(): void {
     //
   }
-
+  private createForm(): FormGroup {
+    const form = this.fb.group({
+      unitNumber: ['', [Validators.required]],
+      requesterName: ['', [Validators.required]],
+      requesterEmail: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+$'),
+        ],
+      ],
+      serviceType: ['', [Validators.required]],
+      summary: ['', [Validators.required]],
+      details: [''],
+    });
+    return form;
+  }
 }
