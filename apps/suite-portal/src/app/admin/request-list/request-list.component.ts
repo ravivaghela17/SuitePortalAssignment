a6dd664d8a0497350c8aad29b6176ab9325d5598
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-05-22 12:18:51 -0400

    build : integrated get request api

--- apps/suite-portal/src/app/admin/request-list/request-list.component.ts
+++ apps/suite-portal/src/app/admin/request-list/request-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MaintenanceRequestApiService } from '../../services/maintenance-api.service';
+import { catchError, first, map, tap } from 'rxjs/operators';
+import { MaintenanceRequest, Response } from '@suiteportal/api-interfaces';
+import { ERROR_MESSAGES, SNACK_BAR_TYPES } from '../../constants';
+import { UtilService } from '../../services/util.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'sp-request-list',
@@ -6,7 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-list.component.scss'],
 })
 export class RequestListComponent implements OnInit {
-  constructor() {}
+  displayedColumns: string[] = [
+    'name',
+    'email',
+    'unitNumber',
+    'serviceType',
+    'summary',
+    'details',
+    'action',
+  ];
+  requestes$: Observable<MaintenanceRequest[]>;
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly utilService: UtilService,
+    private readonly maintenanceRequestApiService: MaintenanceRequestApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.requestes$ = this.getAllOpenMaintenanceRequests();
+  }
+  getAllOpenMaintenanceRequests(): Observable<MaintenanceRequest[]> {
+    return this.maintenanceRequestApiService.getOpenMaintenanceRequest().pipe(
+      first(),
+      map(
+        (response: Response) =>
+          (response.data?.requests as MaintenanceRequest[]) ?? []
+      ),
+      catchError((error) => {
+        const errorMessage = error?.message
+          ? error.message
+          : ERROR_MESSAGES.REQUEST_FAILURE;
+        this.utilService.openSnackBar(SNACK_BAR_TYPES.ERROR, errorMessage);
+        return of([]);
+      })
+    );
+  }
 }
--- apps/suite-portal/src/app/services/maintenance-api.service.ts
+++ apps/suite-portal/src/app/services/maintenance-api.service.ts
@@ -3,12 +3,16 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { MaintenanceRequest, Response } from '@suiteportal/api-interfaces';
 
+const MAINTENANCE_REQUEST = '/api/maintenance-requests';
 @Injectable({
   providedIn: 'root',
 })
 export class MaintenanceRequestApiService {
   constructor(private readonly http: HttpClient) {}
   createMaintenanceRequest(body: MaintenanceRequest): Observable<Response> {
-    return this.http.post<Response>('/api/maintenance-requests', body);
+    return this.http.post<Response>(MAINTENANCE_REQUEST, body);
+  }
+  getOpenMaintenanceRequest(): Observable<Response> {
+    return this.http.get<Response>(MAINTENANCE_REQUEST, {});
   }
 }
