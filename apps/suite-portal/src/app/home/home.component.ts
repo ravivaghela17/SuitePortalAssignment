494ea81de1827f79df2308f7796a2540e80f3af6
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-05-21 21:28:59 -0400

    Build : added getters and functions to show proper error message

--- apps/suite-portal/src/app/home/home.component.ts
+++ apps/suite-portal/src/app/home/home.component.ts
@@ -5,8 +5,12 @@ import {
   Validators,
   FormBuilder,
   FormControl,
+  AbstractControl,
 } from '@angular/forms';
-
+const ERROR_MESSAGES = {
+  REQUIRED_FIELD: 'This field is required',
+  EMAIL_PATTERN: 'Please enter a valid email address',
+};
 @Component({
   selector: 'pm-home',
   templateUrl: './home.component.html',
@@ -15,12 +19,37 @@ import {
 export class HomeComponent implements OnInit {
   serviceTypes = ALL_SERVICE_TYPES;
   form: FormGroup;
-  constructor(private readonly fb: FormBuilder) {
-    this.form = this.createForm();
-  }
+  constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit(): void {
-    //
+    this.form = this.createForm();
+  }
+  get unitNumber(): AbstractControl {
+    return this.form.get('unitNumber');
+  }
+  get requesterName(): AbstractControl {
+    return this.form.get('requesterName');
+  }
+  get requesterEmail(): AbstractControl {
+    return this.form.get('requesterEmail');
+  }
+  get serviceType(): AbstractControl {
+    return this.form.get('serviceType');
+  }
+  get summary(): AbstractControl {
+    return this.form.get('summary');
+  }
+  getRequiredFieldMessage(field: string): string {
+    const errors = this.form.get(field).errors;
+    return errors.required ? ERROR_MESSAGES.REQUIRED_FIELD : '';
+  }
+  getEmailErrorMessage(): string {
+    const errors = this.requesterEmail.errors;
+    return errors.required
+      ? ERROR_MESSAGES.REQUIRED_FIELD
+      : errors.pattern
+      ? ERROR_MESSAGES.EMAIL_PATTERN
+      : '';
   }
   private createForm(): FormGroup {
     const form = this.fb.group({
