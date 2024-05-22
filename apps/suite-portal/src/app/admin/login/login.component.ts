9c2b42799a911f568a8d9f79f603d3fec783e3f1
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-05-22 01:09:16 -0400

    Build : added validations for login page

--- apps/suite-portal/src/app/admin/login/login.component.ts
+++ apps/suite-portal/src/app/admin/login/login.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AuthApiService } from '../../services/auth-api.service';
+import { REGEX_EMAIL_PATTERN, ERROR_MESSAGES } from '../../constants';
+import { UtilService } from '../../services/util.service';
+
 @Component({
   selector: 'sp-login',
   templateUrl: './login.component.html',
@@ -8,9 +16,11 @@ import { AuthApiService } from '../../services/auth-api.service';
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
+  hidePassword = true;
   constructor(
     private readonly fb: FormBuilder,
-    private readonly authApiService: AuthApiService
+    private readonly authApiService: AuthApiService,
+    private readonly utilService: UtilService
   ) {}
   ngOnInit(): void {
     this.createForm();
@@ -18,14 +28,26 @@ export class LoginComponent implements OnInit {
   login() {
     this.authApiService.login(this.form.value).subscribe();
   }
+  get password(): AbstractControl {
+    return this.form.get('password');
+  }
+  get email(): AbstractControl {
+    return this.form.get('email');
+  }
+  getEmailErrorMessage(): string {
+    const errors = this.email.errors;
+    return this.utilService.getEmailErrorMessage(errors);
+  }
+  getPasswordErrorMessage(): string {
+    const errors = this.password.errors;
+    return this.utilService.getRequiredFieldMessage(errors);
+  }
+
   private createForm(): void {
     this.form = this.fb.group({
       email: [
         '',
-        [
-          Validators.required,
-          Validators.pattern('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+$'),
-        ],
+        [Validators.required, Validators.pattern(REGEX_EMAIL_PATTERN)],
       ],
       password: ['', [Validators.required]],
     });
