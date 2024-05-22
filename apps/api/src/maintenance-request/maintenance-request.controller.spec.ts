72f93b8e95c3fa24481570ca212cfd947bcf352e
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-05-22 17:13:26 -0400

    Build : added units to validate create and update maintenance requests

--- apps/api/src/maintenance-request/maintenance-request.controller.spec.ts
+++ apps/api/src/maintenance-request/maintenance-request.controller.spec.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Test, TestingModule } from '@nestjs/testing';
+import { ServiceType, RequestStatus } from '@suiteportal/api-interfaces';
 import { MaintenanceRequestController } from './maintenance-request.controller';
 import { MaintenanceRequestService } from './maintenance-request.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('MaintenanceRequestController', () => {
   let controller: MaintenanceRequestController;
@@ -15,10 +17,12 @@ describe('MaintenanceRequestController', () => {
       controllers: [MaintenanceRequestController],
       providers: [
         { provide: MaintenanceRequestService, useValue: mockService },
-      ]
+      ],
     }).compile();
 
-    controller = module.get<MaintenanceRequestController>(MaintenanceRequestController);
+    controller = module.get<MaintenanceRequestController>(
+      MaintenanceRequestController
+    );
   });
 
   it('should be defined', () => {
@@ -26,11 +30,51 @@ describe('MaintenanceRequestController', () => {
   });
 
   it('should call dao when getting a maintenance request by id', async () => {
-    mockService.getMaintenanceRequest = jest.fn().mockResolvedValue({ id: '1', summary: 'test' });
+    mockService.getMaintenanceRequest = jest
+      .fn()
+      .mockResolvedValue({ id: '1', summary: 'test' });
 
     const result = await controller.getMaintenanceRequest('1');
 
     expect(mockService.getMaintenanceRequest).toHaveBeenCalled();
-    expect(result).toEqual({ id: '1', summary: 'test'});
+    expect(result).toEqual({ id: '1', summary: 'test' });
+  });
+
+  it('should call service when creating a maintenance request', async () => {
+    const request = {
+      name: 'test',
+      email: 'email@email.com',
+      unitNumber: '123',
+      summary: 'test',
+      serviceType: ServiceType.General,
+      status: RequestStatus.open,
+    };
+    mockService.createMaintenanceRequest = jest
+      .fn()
+      .mockResolvedValue({ id: '1', ...request });
+
+    const result = await controller.createMaintenanceRequest(request);
+
+    expect(mockService.createMaintenanceRequest).toHaveBeenCalledWith(request);
+    expect(result).toEqual({ id: '1', ...request });
+  });
+  it('should throw BadRequestException if no id is provided', async () => {
+    await expect(controller.closeMaintenanceRequest('')).rejects.toThrow(
+      BadRequestException
+    );
+  });
+
+  it('should call service when updating a maintenance request', async () => {
+    const request = {
+      id: '1',
+      summary: 'updated test',
+      stauts: RequestStatus.close,
+    };
+    mockService.closeMaintenanceRequest = jest.fn().mockResolvedValue(request);
+
+    const result = await controller.closeMaintenanceRequest('1');
+
+    expect(mockService.closeMaintenanceRequest).toHaveBeenCalledWith('1');
+    expect(result).toEqual(request);
   });
 });
