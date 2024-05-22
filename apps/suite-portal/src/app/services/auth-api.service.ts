20e6af8c36f4eba4ef1ab4ab5e25c0572c762466
Ravi Vaghela <rvaghela@fleetoperate.com>
2024-05-22 10:53:17 -0400

    Build : storing session when admin logs in

--- apps/suite-portal/src/app/services/auth-api.service.ts
+++ apps/suite-portal/src/app/services/auth-api.service.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User, Response } from '@suiteportal/api-interfaces';
+import { tap } from 'rxjs/operators';
+
+class Storage {
+  key: string;
+  object: any;
+  constructor(key: string, object: any) {
+    this.key = key;
+    this.object = object;
+  }
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +19,15 @@ import { User, Response } from '@suiteportal/api-interfaces';
 export class AuthApiService {
   constructor(private readonly http: HttpClient) {}
   login(body: User): Observable<Response> {
-    return this.http.post<Response>('/api/admin/login', body);
+    return this.http.post<Response>('/api/admin/login', body).pipe(
+      tap((response) => {
+        if (response.data) {
+          //clearing existing session
+          sessionStorage.clear();
+          const storage = new Storage('ADMIN', response.data);
+          sessionStorage.setItem('ADMIN', JSON.stringify(storage));
+        }
+      })
+    );
   }
 }
